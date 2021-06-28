dc5d1078dea0257caeabdb457e516f03b89fddea
sitoulabibek55 <61705086+sitoulabibek55@users.noreply.github.com>
2021-06-28 16:36:33 -0500

    Update withdrawFunds.java

--- src/withdrawFunds.java
+++ src/withdrawFunds.java
@@ -3,7 +3,10 @@ public class withdrawFunds {
 	
 	//Withdraws funds by taking the account number and withdrawl amount as input
 	//passing them to bankingSystem and bankServer, then returning confirmation
-	public static void withdraw(int acctnum, double withdrawl) {
-		
+	public static void withdraw(int acctnum, double amount) {
+		double balance;
+		System.out.println("Enter Withdraw amount: "+ amount);
+		balance =- amount;
+		System.out.println("Remaining Balance of the account is: " + balance);
 	}
 }
